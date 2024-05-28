8088e41c292bb220c70b7972d8d4a40270d0d164
Jaouadjj <165282739+Jaouadjj@users.noreply.github.com>
2024-05-28 10:01:13 +0200

    d

---
+++ src/main/java/com/example/project2/AiComponent.java
@@ -0,0 +1,25 @@
+package com.example.project2;
+
+import java.util.Random;
+
+public class AiComponent {
+
+    private String[] Antwoorden = {
+            "We zijn momenteel offline.",
+            "Sorry, ik kan nu niet antwoorden.",
+            "De service is momenteel niet beschikbaar.",
+            "We zijn momenteel bezig met onderhoud.",
+            "Ik ben momenteel niet bereikbaar."
+    };
+
+    private Random random;
+
+    public AiComponent() {
+        this.random = new Random();
+    }
+
+    public String getAntwoord() {
+        int index = random.nextInt(Antwoorden.length);
+        return Antwoorden[index];
+    }
+}
