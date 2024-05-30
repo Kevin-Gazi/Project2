8df40a0c3daae94c96e2bab133bda53d45427202
kurty <kurtyek17@gmail.com>
2024-05-30 14:32:14 +0200

    ChatObservable

---
+++ src/main/java/com/example/project2/ChatObservable.java
@@ -0,0 +1,17 @@
+package com.example.project2;
+
+import java.util.List;
+import java.util.Observer;
+
+public class ChatObservable {
+    public List observers;
+
+
+    public void addObservers (Observer observer) {
+
+    }
+
+    public void notiyObservers() {
+
+    }
+}
