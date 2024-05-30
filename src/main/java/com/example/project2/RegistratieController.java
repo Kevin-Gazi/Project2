617aa6324cfff5470dc38df541a4485c63c1ada9
faris <farishalepoto@gmail.com>
2024-05-30 13:31:05 +0200

    Registratie class

--- src/main/java/com/example/project2/RegistratieController.java
+++ src/main/java/com/example/project2/RegistratieController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.*;
 
 import java.util.ArrayList;
 
-public class RegistratieController {
+public class RegistratieController extends Registratie {
     public ArrayList <Gebruiker> gebruikers;
     @FXML
     private TextField gebruikersnaamRegistratie;
