98011e7a58590e58a2dd2e8936556975fa66043d
Jaouadjj <165282739+Jaouadjj@users.noreply.github.com>
2024-05-31 09:50:49 +0200

    Instellingen klas

---
+++ src/main/java/com/example/project2/Instellingen.java
@@ -0,0 +1,21 @@
+package com.example.project2;
+
+public class Instellingen {
+
+    public void updateGebruikersnaam(){
+    }
+
+    public void updateGebruikerWachtwoord(){
+
+    }
+
+    public void updateGebruikerEmail(){
+
+    }
+
+    public void updateStandaardTaal(){
+
+    }
+
+
+}
