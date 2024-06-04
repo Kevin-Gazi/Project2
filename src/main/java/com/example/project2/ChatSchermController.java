fa27973d747bc59a8ddbb672877eff8328ffc6e2
Jaouadjj <165282739+Jaouadjj@users.noreply.github.com>
2024-06-04 21:13:05 +0200

    chat scherm

---
+++ src/main/java/com/example/project2/ChatSchermController.java
@@ -0,0 +1,64 @@
+package com.example.project2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ChatSchermController {
+    @FXML
+    private TabPane tabPane;
+    @FXML
+    private TextField chatNameInput;
+
+
+    public void sendChat1(ActionEvent event) {
+        TextArea chatArea = (TextArea) tabPane.getTabs().get(0).getContent().lookup("#chatArea1");
+        TextField chatInput = (TextField) tabPane.getTabs().get(0).getContent().lookup("#chatInput1");
+        chatArea.appendText("Gebruiker: " + chatInput.getText() + "\n");
+        chatInput.clear();
+    }
+
+
+
+    public void addNewTab(ActionEvent event) {
+        int tabCount = tabPane.getTabs().size();
+        Tab newTab = new Tab("Chat " + (tabCount + 1));
+
+
+        String chatName = chatNameInput.getText();
+        if (chatName.isEmpty()) {
+            chatName = "Chat " + (tabPane.getTabs().size() + 1);
+        }
+        newTab.setText(chatName);
+
+
+
+        TextArea chatArea = new TextArea();
+        chatArea.setPrefHeight(400);
+        chatArea.setPrefWidth(650);
+
+        TextField chatInput = new TextField();
+        chatInput.setPrefWidth(550);
+
+        Button sendButton = new Button("Stuur");
+        sendButton.setOnAction(e -> {
+            chatArea.appendText("Gebruiker: " + chatInput.getText() + "\n");
+            chatInput.clear();
+        });
+
+        AnchorPane content = new AnchorPane();
+        content.getChildren().addAll(chatArea, chatInput, sendButton);
+        AnchorPane.setTopAnchor(chatArea, 14.0);
+        AnchorPane.setLeftAnchor(chatArea, 14.0);
+        AnchorPane.setRightAnchor(chatArea, 14.0);
+        AnchorPane.setBottomAnchor(chatInput, 14.0);
+        AnchorPane.setLeftAnchor(chatInput, 14.0);
+        AnchorPane.setRightAnchor(chatInput, 14.0);
+        AnchorPane.setBottomAnchor(sendButton, 14.0);
+        AnchorPane.setRightAnchor(sendButton, 14.0);
+
+        newTab.setContent(content);
+        tabPane.getTabs().add(newTab);
+    }
+}
